dc6d8071d69d7974df06e4440f673a9488cabd67
hivevu <hivevu@gmail.com>
2019-05-24 23:08:12 +0800

    add: 首页

--- mini/pages/index/index.js
+++ mini/pages/index/index.js
@@ -9,14 +9,75 @@ Page({
       './assets/swiper.png',
       './assets/swiper.png',
     ],
-    indicatorDots: false,
+    indicatorDots: true,
+    indicatorColor: '#DCDCDC',
+    indicatorActiveColor: '#FF646C',
     autoplay: false,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    // 热门活动列表
+    hotActivityList: [
+      './assets/swiper1.png',
+      './assets/swiper2.png',
+    ],
+    // 
+    rightMargin: '750rpx',
+    // 线下活动
+    offlineActivityList: [{
+        img: './assets/swiper2.png',
+        header: '这里是标题这里是标题这里是标题这里是标题这里是标题这里是标题',
+        date: '2019-05-21'
+      },
+      {
+        img: './assets/swiper2.png',
+        header: '这里是标题这里是标题这里是标题这里是标题这里是标题这里是标题',
+        date: '2019-05-21'
+      }, {
+        img: './assets/swiper2.png',
+        header: '这里是标题这里是标题这里是标题这里是标题这里是标题这里是标题',
+        date: '2019-05-21'
+      }
+    ],
+    // 我要投稿
+    recruitmentList: [{
+        img: './assets/swiper2.png',
+        header: '这里是标题这里是标题这里是标题这里是标题这里是标题这里是标题',
+        date: '2019-05-21',
+        number: 1999
+      },
+      {
+        img: './assets/swiper2.png',
+        header: '这里是标题这里是标题这里是标题这里是标题这里是标题这里是标题',
+        date: '2019-05-21',
+        number: 1999
+      }, {
+        img: './assets/swiper2.png',
+        header: '这里是标题这里是标题这里是标题这里是标题这里是标题这里是标题',
+        date: '2019-05-21',
+        number: 1999
+      }
+    ],
+    // 我要助力
+    helpList: [{
+        img: './assets/swiper2.png',
+      },
+      {
+        img: './assets/swiper2.png',
+      },
+      {
+        img: './assets/swiper2.png',
+      },
+      {
+        img: './assets/swiper2.png',
+      },
+    ]
   },
 
   onLoad: function() {
-
+    const length = -(this.data.hotActivityList.length - 2) * 75 + 750
+    this.setData({
+      rightMargin: length + 'rpx'
+    })
   },
 
 })
