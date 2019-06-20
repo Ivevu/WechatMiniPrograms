f542bcecb50af3dc0483ec21d779c31e58127a35
ivevu <hivevu@gmail.com>
2019-06-20 18:36:28 +0800

    fix: 首页 征稿 更多按钮 接口更换

--- mini/pages/recruitment/recruitment.js
+++ mini/pages/recruitment/recruitment.js
@@ -2,6 +2,8 @@ import {
   api
 } from '../../config/config.js';
 const app = getApp();
+import Http from '../../utils/http.js';
+const http = new Http();
 
 Page({
   data: {
@@ -15,7 +17,7 @@ Page({
       url: `../offActDetail/offActDetail?id=${id}&type=2`
     });
   },
-  // 获取线下活动列表
+  // 获取已投征稿活动列表
   getActivityList(type, openId) {
     wx.request({
       url: api.enrolment,
@@ -24,33 +26,50 @@ Page({
         openId
       },
       success: res => {
-        if (!res.data.data) return;
-        let list = res.data.data;
-        let on = [];
-        list.forEach(item => {
-          if (item.deadline) {
-            item.deadline = '征稿截止时间：' + item.deadline.substring(0, 10);
-          }
-          if (item.activityState == 1) {
-            on.push(item);
-          }
-        })
-        this.setData({
-          recruitmentList: on
-        });
+        this.setList(res);
       }
     });
   },
+  /**
+   * @description 获取所有的征稿列表
+   */
+  getRecList() {
+    http
+      ._get(api.list, {
+        type: 2
+      })
+      .then(res => {
+        this.setList(res);
+      })
+  },
+  setList(res) {
+    if (!res.data.data) return;
+    let list = res.data.data;
+    let on = [];
+    list.forEach(item => {
+      if (item.deadline) {
+        item.deadline = '征稿截止时间：' + item.deadline.substring(0, 10);
+      }
+      if (item.activityState == 1) {
+        on.push(item);
+      }
+    })
+    this.setData({
+      recruitmentList: on
+    });
+  },
   onLoad(options) {
+    if (!options.hasOwnProperty('path')) return;
     this.setData({
       path: options.path
     });
   },
   onShow: function(options) {
-    if (this.data.path) {
+    console.log(this.data)
+    if (!!this.data.path) {
       this.getActivityList(2, app.globalData.openId)
     } else {
-      this.getActivityList(2);
+      this.getRecList();
     }
   },
 })
